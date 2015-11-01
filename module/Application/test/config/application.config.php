ce95b244636638ed5fb3acdc6f1037755e5e3ca4
kuai6 <kuai6@ya.ru>
2015-11-01 19:08:10 +0300

    add phpunit tests

---
+++ module/Application/test/config/application.config.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    // This should be an array of module namespaces used in the application.
+    'modules' => [
+        'Application',
+    ],
+
+    // These are various options for the listeners attached to the ModuleManager
+    'module_listener_options' => [
+        // This should be an array of paths in which modules reside.
+        // If a string key is provided, the listener will consider that a module
+        // namespace, the value of that key the specific path to that module's
+        // Module class.
+        'module_paths' => [
+            __DIR__ . '/../../../../module',
+            __DIR__ . '/../../../../vendor',
+        ],
+    ],
+];
