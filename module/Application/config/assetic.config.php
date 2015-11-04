e377babf83d78bf4ebf46e153580927d81f5ab8a
kuai6 <kuai6@ya.ru>
2015-11-04 20:13:44 +0300

    add inspinia template theme

--- module/Application/config/assetic.config.php
+++ module/Application/config/assetic.config.php
@@ -5,6 +5,13 @@ namespace Application;
 return [
     'assetic_configuration' => [
 
+        'default' => array(
+            'assets' => array(
+                '@head_common_js',
+                '@head_common_css',
+            ),
+        ),
+
 //        'routes' => array(
 //            'home' => array(
 //                '@common_css',
@@ -12,30 +19,51 @@ return [
 //            ),
 //        ),
 
-        'controllers' => [
-            'Application\Controller\Index' => [
-                '@common_css',
-                '@common_js',
-            ],
-        ],
+//        'controllers' => [
+//            'Application\Controller\Index' => [
+//                '@common_css',
+//                '@common_js',
+//            ],
+//        ],
         'modules' => [
             __NAMESPACE__ => [
                 'root_path' => __DIR__ . '/../assets',
                 'collections' => [
-                    'common_css' => [
+                    'head_common_css' => [
                         'assets' => [
                             'css/bootstrap.min.css',
                             'css/animate.css',
+                            'css/font-awesome.css',
+                            'css/style.css',
                         ],
                     ],
-                    'common_js' => [
+                    'head_common_js' => [
                         'assets' => [
-                            'js/bootstrap.min.js',
                             'js/jquery-2.1.1.js',
                             'js/jquery-ui-1.10.4.min.js',
                             'js/jquery-ui.custom.min.js',
+                            'js/bootstrap.min.js',
                         ],
                     ],
+                    'common_images' => array(
+                        'assets' => array(
+                            'images/*',
+                            'images/gallery/*',
+                            'images/landing/*',
+
+                        ),
+                        'options' => array(
+                            'move_raw' => true,
+                        )
+                    ),
+                    'common_fonts' => array(
+                        'assets' => array(
+                            'fonts/*',
+                        ),
+                        'options' => array(
+                            'move_raw' => true,
+                        )
+                    ),
                 ],
             ],
         ],
