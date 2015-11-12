21d1f84b717affb1055d97f89f77535913d8fab6
kuai6 <kuai6@ya.ru>
2015-11-12 23:52:59 +0300

    implement  contact closure daemon

--- module/Application/src/Command/ContactClosure.php
+++ module/Application/src/Command/ContactClosure.php
@@ -14,22 +14,22 @@ class ContactClosure extends AbstractCommand
     public function getAllStatuses()
     {
         $this->sequence = [self::CONTROL_COMMAND, 175, 0, 7];
-        $packFormat = str_repeat('C', count($this->sequence));
+        $packFormat = 'C*';
         $request = call_user_func_array('pack', array_merge([$packFormat], $this->sequence));
         $this->getAdapter()->write($request);
         $response = $this->getAdapter()->read();
         $result = [];
         for ($i = 0; $i < mb_strlen($response); $i++) {
             $buf = unpack("C", $response[$i])[1];
-
-            $result[$i][] = ($buf & 1)  ==  1   ? 1 : 0;
-            $result[$i][] = ($buf & 2)  ==  2   ? 1 : 0;
-            $result[$i][] = ($buf & 4)  ==  4   ? 1 : 0;
-            $result[$i][] = ($buf & 8)  ==  8   ? 1 : 0;
-            $result[$i][] = ($buf & 16) ==  16  ? 1 : 0;
-            $result[$i][] = ($buf & 32) ==  32  ? 1 : 0;
-            $result[$i][] = ($buf & 64) ==  64  ? 1 : 0;
-            $result[$i][] = ($buf & 128)==  128 ? 1 : 0;
+            $bankName = $i+1;
+            $result[$bankName][] = ($buf & 1)  ==  1   ? 1 : 0;
+            $result[$bankName][] = ($buf & 2)  ==  2   ? 1 : 0;
+            $result[$bankName][] = ($buf & 4)  ==  4   ? 1 : 0;
+            $result[$bankName][] = ($buf & 8)  ==  8   ? 1 : 0;
+            $result[$bankName][] = ($buf & 16) ==  16  ? 1 : 0;
+            $result[$bankName][] = ($buf & 32) ==  32  ? 1 : 0;
+            $result[$bankName][] = ($buf & 64) ==  64  ? 1 : 0;
+            $result[$bankName][] = ($buf & 128)==  128 ? 1 : 0;
         }
 
         return $result;
