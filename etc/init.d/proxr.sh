6bcf78e6313ed2f0be2b286a6c74ea1dbe61d55a
kuai6 <kuai6@ya.ru>
2015-11-16 20:08:52 +0300

    fic sh script

--- etc/init.d/proxr.sh
+++ etc/init.d/proxr.sh
@@ -3,6 +3,7 @@
 uid="kuai6";
 php=/usr/bin/php
 installationName="proxr"
+CONSOLETYPE="normal"
 
 if [[ $USER != $uid ]]; then
     if [[ $EUID -ne 0 ]]; then
@@ -14,7 +15,67 @@ if [[ $USER != $uid ]]; then
     fi;
 fi;
 
-. /etc/init.d/functions
+# This all seem confusing? Look in /etc/sysconfig/init,
+# or in /usr/share/doc/initscripts-*/sysconfig.txt
+RES_COL=60
+MOVE_TO_COL="echo -en \\033[${RES_COL}G"
+SETCOLOR_SUCCESS="echo -en \\033[1;32m"
+SETCOLOR_FAILURE="echo -en \\033[1;31m"
+SETCOLOR_WARNING="echo -en \\033[1;33m"
+SETCOLOR_NORMAL="echo -en \\033[0;39m"
+
+if [ "$CONSOLETYPE" = "serial" ]; then
+  MOVE_TO_COL=
+  SETCOLOR_SUCCESS=
+  SETCOLOR_FAILURE=
+  SETCOLOR_WARNING=
+  SETCOLOR_NORMAL=
+fi
+
+
+echo_success() {
+  [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+  echo -n "["
+  [ "$BOOTUP" = "color" ] && $SETCOLOR_SUCCESS
+  echo -n $"  OK  "
+  [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
+  echo -n "]"
+  echo -ne "\r"
+  return 0
+}
+
+echo_failure() {
+  [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+  echo -n "["
+  [ "$BOOTUP" = "color" ] && $SETCOLOR_FAILURE
+  echo -n $"FAILED"
+  [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
+  echo -n "]"
+  echo -ne "\r"
+  return 1
+}
+
+echo_passed() {
+  [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+  echo -n "["
+  [ "$BOOTUP" = "color" ] && $SETCOLOR_WARNING
+  echo -n $"PASSED"
+  [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
+  echo -n "]"
+  echo -ne "\r"
+  return 1
+}
+
+echo_warning() {
+  [ "$BOOTUP" = "color" ] && $MOVE_TO_COL
+  echo -n "["
+  [ "$BOOTUP" = "color" ] && $SETCOLOR_WARNING
+  echo -n $"WARNING"
+  [ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
+  echo -n "]"
+  echo -ne "\r"
+  return 1
+}
 
 prefix="/srv/www/$installationName"
 
