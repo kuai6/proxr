929bea2cb646a51759af06051b0c972033dd33c8
kuai6 <kuai6@ya.ru>
2019-03-17 13:47:26 +0300

    fix repository

--- module/Application/src/EntityRepository/Periphery.php
+++ module/Application/src/EntityRepository/Periphery.php
@@ -9,19 +9,12 @@ class Periphery extends EntityRepository
 {
     public function findByBit($deviceId, $bankId, $bit)
     {
-        return $this->findOneBy(
-            Criteria::create()
-                ->where(Criteria::expr()->eq('device_id', $deviceId))
-                ->andWhere(Criteria::expr()->eq('bank_id', $bankId))
-                ->andWhere(Criteria::expr()->eq('bit', $bit))
-        );
+        return $this->findOneBy(['device_id' => $deviceId, 'bank_id' => $bankId, 'bit' => $bit]);
     }
 
     public function findByDevice($device_id)
     {
-        return $this->findBy(
-            Criteria::create()
-                ->where(Criteria::expr()->eq('device_id', $device_id)));
+        return $this->findBy(['device_id', $device_id]);
     }
 
     public function save($entity)
